6aa79b335073fa348d206550ff0e35119b22c5dc
sim4r4 <me@miguelaramis.com>
2025-11-19 08:39:12 -0800

    Update formula to v1.0.2
    - Fix version command to read from package.json
    - Update SHA256 checksum

--- oio.rb
+++ oio.rb
@@ -1,8 +1,8 @@
 class Oio < Formula
   desc "CLI for managing notes and ephemeral shorts with authenticated access"
   homepage "https://github.com/sim4gh/oio-cli"
-  url "https://github.com/sim4gh/oio-cli/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "74de5248df64519b935349426c978603d5b4afdd43c4278c11a7342be6f4a19f"
+  url "https://github.com/sim4gh/oio-cli/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "790899ddb644c210a763c1c242b8ec4250ebcef597db34a86cc7d35cb4d74dbd"
   license "ISC"
 
   depends_on "node"
@@ -19,6 +19,6 @@ class Oio < Formula
   end
 
   test do
-    assert_match "1.0.1", shell_output("#{bin}/oio --version")
+    assert_match "1.0.2", shell_output("#{bin}/oio --version")
   end
 end
